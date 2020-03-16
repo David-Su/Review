34d31fbfae225155b76f2dbe495b0e8c30dc3902
SuK <543879736@qq.com>
2020-03-16 17:56:51 +0800

    .

--- Android/Practice/app/src/main/java/suk/practice/MainActivity.kt
+++ Android/Practice/app/src/main/java/suk/practice/MainActivity.kt
@@ -1,17 +1,11 @@
 package suk.practice
 
-import android.app.Activity
-import android.app.Instrumentation
 import android.content.Intent
+import android.os.Binder
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import android.view.WindowManager
-
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -21,7 +15,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         btn.setOnClickListener {
             startActivity(Intent(this,MainActivity2::class.java))
-            Handler().post {  Log.d("startActivity","Handler1") }
+            Handler().post {
+//                while (true) {
+//
+//                }
+
+                Log.d("startActivity","Handler1")
+            }
             Log.d("startActivity","执行后")
         }
 
