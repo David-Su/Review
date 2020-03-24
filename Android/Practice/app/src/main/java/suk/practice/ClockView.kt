f8247098a5a9b46024d22372afd4f99e5d66f615
SuK <543879736@qq.com>
2020-03-24 17:59:29 +0800

    .

--- Android/Practice/app/src/main/java/suk/practice/ClockView.kt
+++ Android/Practice/app/src/main/java/suk/practice/ClockView.kt
@@ -1,8 +1,19 @@
 package suk.practice
 
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
+import android.icu.util.Measure
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
+import java.util.*
+import kotlin.concurrent.timer
+import kotlin.math.log
 
 /**
  * @author SuK
@@ -11,8 +22,55 @@ import android.view.View
  */
 class ClockView : View {
 
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {
 
+    private val mPaint = Paint()
+    private val mTag = "ClockView"
+    private var secDegress = 0f
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+//        context.registerReceiver(object : BroadcastReceiver() {
+//            override fun onReceive(context: Context, intent: Intent?) {
+//
+//            }
+//
+//        }, IntentFilter(Intent.ACTION_TIME_TICK))
+
+        timer(period = 1000L) {
+            val hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+            val minute = Calendar.getInstance().get(Calendar.MINUTE)
+            val second = Calendar.getInstance().get(Calendar.SECOND)
+
+            secDegress = second.toFloat() / 60f * 360f
+
+            invalidate()
+
+            Log.d(mTag, second.toString())
+        }
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val width = MeasureSpec.getSize(widthMeasureSpec)
+        val height = MeasureSpec.getSize(heightMeasureSpec)
+        val size = if (width > height) height else width
+        super.onMeasure(
+            MeasureSpec.makeMeasureSpec(size, MeasureSpec.EXACTLY),
+            MeasureSpec.makeMeasureSpec(size, MeasureSpec.EXACTLY)
+        )
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        val size= height.toFloat()
+        val r= height/2f
+        mPaint.flags = Paint.ANTI_ALIAS_FLAG
+        mPaint.strokeWidth = context.resources.getDimension(R.dimen.dp_5)
+        mPaint.color = context.getColor(R.color.colorPrimary)
+        mPaint.style = Paint.Style.STROKE
+        canvas.drawArc(RectF(0f,0f,size,size),0f,360f,true,mPaint)
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
     }
 
 }
