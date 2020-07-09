0f950e971ac9c3a644fa1ba59720fd2fae401b99
SuK <543879736@qq.com>
2020-07-09 17:54:28 +0800

    .

--- Android/Practice/app/src/main/java/suk/practice/MainActivity.kt
+++ Android/Practice/app/src/main/java/suk/practice/MainActivity.kt
@@ -47,7 +47,7 @@ class MainActivity : BaseActivity() {
         }
         FixClass().run(this)
         btn2.setOnClickListener {
-
+HashSet
             FixClass().run(this)
         }
         bindService(Intent(this, ServerService::class.java), object : ServiceConnection {
--- Android/Practice/java/src/main/java/com/practice/java/algorithm/sort/InsertSort.java
+++ Android/Practice/java/src/main/java/com/practice/java/algorithm/sort/InsertSort.java
@@ -11,7 +11,7 @@ public class InsertSort implements Strategy {
     @Override
     public int[] sort(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
-            int temp = array[i + 1];
+            int temp = array[i + 1]; //这是一个基准数，在它之前的数组是有序的，即index之前的数组是有序的，算法要做的就是把这个基准数插入到index之前的有序数组中。
             int index = i;
             while (index >= 0 && array[index] > temp) {
                 array[index + 1] = array[index];
