8fec6003bcec557ca2930bdc45d862351d7a556d
sukai <sukai@gamegoing.net>
2022-06-26 21:22:55 +0800

    commit

--- Android/Practice/java/src/main/java/com/practice/java/algorithm/sort/BubbleSort.java
+++ Android/Practice/java/src/main/java/com/practice/java/algorithm/sort/BubbleSort.java
@@ -8,7 +8,7 @@ package com.practice.java.algorithm.sort;
 public class BubbleSort implements Strategy {
 
     //执行次数：
-    //设数组大小为n，(4n-1)+(4n-2)+...+2+1=4n*(n/2)=(n^2)/2
+    //设数组大小为n，4*(n-1)+4*(n-2)+...+2+1 总共相加n-1次 = 4*（(n-1)+(n-2)+...+2+1）= n*(n-1)/2
     //时间复杂度：O(n^2)
     public int[] sort(int[] array) {
         for (int i = array.length; i > 0; i--) {
--- Android/Practice/java/src/main/java/com/practice/java/algorithm/sort/InsertSort.java
+++ Android/Practice/java/src/main/java/com/practice/java/algorithm/sort/InsertSort.java
@@ -13,7 +13,7 @@ public class InsertSort implements Strategy {
         for (int i = 0; i < array.length - 1; i++) {
             int temp = array[i + 1]; //这是一个基准数，在它之前的数组是有序的，即index之前的数组是有序的，算法要做的就是把这个基准数插入到index之前的有序数组中。
             int index = i;
-            while (index >= 0 && array[index] > temp) {
+            while (index >= 0 && array[index] > temp) {//基准数之前的数字逐个与基准数对比，如果大于基准数则向前移动一位，否则停止循环
                 array[index + 1] = array[index];
                 index--;
             }
